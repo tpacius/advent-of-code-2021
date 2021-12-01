f84330f1f9474e7f8e1df680664dee79515584ea
Tim Pacius <tpacius@gmail.com>
2021-12-01 12:18:48 -0500

    Day 1 part 2

--- day1/main.js
+++ day1/main.js
@@ -17,4 +17,26 @@ for (let data of rawData) {
   prev = data;
 }
 
-console.log(count);
+console.log(`Part 1 ${count}`);
+
+let data = rawData.map((x) => parseInt(x));
+
+let sumCount = 0;
+
+function numberOfGreaterSums(arr, window) {
+  let prevSum = 0;
+  let currentSum = 0;
+  for (let i = 0; i < window; i++) {
+    currentSum += arr[i];
+  }
+  prevSum = currentSum;
+  for (let i = window; i < arr.length; i++) {
+    currentSum = prevSum - arr[i - window] + arr[i];
+    if (currentSum > prevSum) {
+      sumCount++;
+    }
+  }
+}
+
+numberOfGreaterSums(data, 3);
+console.log(`Part 2 ${sumCount}`);
